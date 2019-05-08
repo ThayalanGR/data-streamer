9b27573b701462c8d5152b633d09a20ea2cd1c43
ThayalanGR <grthayalan18@gmail.com>
2019-05-08 10:48:08 +0530

    video player on unmount bug fixed and player complete functionality over

--- web-front-end/src/components/Player.jsx
+++ web-front-end/src/components/Player.jsx
@@ -21,7 +21,9 @@ class Player extends Component {
       volume: 0.50,
       currentTime: 0,
       curtimetext: '',
-      durtimetext: ''
+      durtimetext: '',
+      majorFading: '',
+      volumeMemory: 0.5
     }
 
     this.closePlayer = this.closePlayer.bind(this);
@@ -33,6 +35,7 @@ class Player extends Component {
     this.initiateVideoInfoHandler = this.initiateVideoInfoHandler.bind(this);
     this.videoTimeUpdater = this.videoTimeUpdater.bind(this);
     this.mainProgressHandler = this.mainProgressHandler.bind(this);
+    this.hideHandler = this.hideHandler.bind(this);
   }
 
   componentWillMount() {
@@ -52,7 +55,23 @@ class Player extends Component {
       this.setState({ isLoading: false });
     }, 1000)
 
+  }
+
+  hideHandler() {
+    this.setState({ majorFading: '' })
+    setTimeout(function () {
+      this.setState({ majorFading: 'vjs-fade-out' })
+    }.bind(this), 3000)
+  }
+
+  initiateVideoInfoHandler() {
+    let video = document.getElementById('videoPlayer');
+    video.addEventListener("timeupdate", this.videoTimeUpdater);
+
+    this.hideHandler();
+
     document.body.onkeydown = (e) => {
+      this.hideHandler();
       if (e.keyCode === 32) {
         this.togglePlay()
       }
@@ -68,23 +87,35 @@ class Player extends Component {
       if (e.keyCode === 40) {
         this.volumeDownHandler()
       }
-
-
     }
 
-  }
+    document.body.ondblclick = (e) => {
+      this.toggleFullScreen()
+    }
 
-  componentWillUnmount() {
-    // var video = document.getElementById("videoPlayer");
-    // video.removeEventListener('timeupdate', funcA);
-  }
+    document.body.onmouseout = function () {
+      this.setState({ majorFading: 'vjs-fade-out' })
+    }.bind(this)
 
-  initiateVideoInfoHandler() {
-    let video = document.getElementById('videoPlayer');
-    video.addEventListener("timeupdate", this.videoTimeUpdater);
+    document.body.onmouseenter = function () {
+      this.hideHandler();
+    }.bind(this)
+
+    document.body.onmousemove = function () {
+      this.hideHandler();
+    }.bind(this)
+
+    document.body.onmouseover = function () {
+      this.hideHandler();
+    }.bind(this)
+
   }
 
   closePlayer() {
+    var video = document.getElementById("videoPlayer");
+    if (!video.paused) {
+      video.pause();
+    }
     this.props.history.goBack()
   }
 
@@ -124,6 +155,18 @@ class Player extends Component {
     let video = document.getElementById('videoPlayer');
     video.volume = value;
     this.setState({ volume: video.volume })
+  }
+
+  volumeMuteHandler() {
+    let video = document.getElementById('videoPlayer');
+    if(this.state.volume === 0) {
+      video.volume = this.state.volumeMemory
+      this.setState({ volume: this.state.volumeMemory })
+    } else {
+      this.setState({volumeMemory: this.state.volume})
+      video.volume = 0.0;
+      this.setState({ volume: 0.0 })
+    }
   }
 
   volumeUpHandler() {
@@ -185,29 +228,29 @@ class Player extends Component {
         <div className="player-wrapper d-flex justify-content-center align-items-center">
           <Spinner size={{ size: "large" }} />
         </div> :
-        <div className="player-wrapper d-flex justify-content-center align-items-center">
-          <div className="logo-icon">
+        <div id="main-body" className={`${this.state.majorFading === '' ? '' : 'hide-cursor'} player-wrapper d-flex justify-content-center align-items-center`}>
+          <div className={`logo-icon ${this.state.majorFading}`}>
             <i onClick={() => { this.props.history.push("/") }} className="fas fa-play text-success logo-size" ></i>
           </div>
-          <div className="float-icon-top">
-            <i onClick={() => { this.toggleFullScreen() }} className={`fas ${this.state.volume > 0.50 ? 'fa-volume-up' : this.state.volume === 0 ? 'fa-volume-mute' : 'fa-volume-down'} hover-green max-icon text-white`}></i>
+          <div className={`float-icon-top ${this.state.majorFading}`}>
+            <i onClick={() => { this.volumeMuteHandler() }} className={`fas ${this.state.volume > 0.50 ? 'fa-volume-up' : this.state.volume === 0 ? 'fa-volume-mute' : 'fa-volume-down'} hover-green max-icon text-white`}></i>
             <span className="text-white mediator ml-2 mr-2">|</span>
             <i onClick={() => { this.toggleFullScreen() }} className={`fa ${this.state.toggleMinMax} hover-green max-icon text-white`}></i>
             <span className="text-white mediator ml-2 mr-2">|</span>
             <i onClick={() => { this.closePlayer() }} className="fa fa-times close-icon hover-green text-white"></i>
           </div>
-          <div className="video-title">
+          <div className={`video-title ${this.state.majorFading}`}>
             {this.state.fileName}
           </div>
 
-          <div className="float-center-controls d-flex justify-content-between align-items-center">
+          <div className={`float-center-controls d-flex ${this.state.majorFading} justify-content-between align-items-center`}>
             <i onClick={() => { this.backwardHandler() }} className="fas fa-undo-alt fa-2x mr-4 hover-green sec-parent"><span className="sec-child" > 10</span></i>
             <i onClick={() => { this.togglePlay() }} className={`fas ${this.state.isPlay ? 'fa-pause' : 'fa-play'} fa-2x ml-4 mr-4 ml-2 mr-2 hover-green`}></i>
             {/* <i className="fas fa-pause fa-2x ml-4 mr-4 hover-green"></i> */}
             <i onClick={() => { this.forwardHandler() }} className="fas fa-redo-alt fa-2x ml-4 hover-green sec-parent"><span className="sec-child" > 10</span></i>
           </div>
 
-          <div className="float-bottom-controls">
+          <div className={`float-bottom-controls ${this.state.majorFading}`}>
             <input onChange={(e) => { this.mainProgressHandler(e.target.value) }} className="progress-bar" type="range" name="progress" min="0" max="100" step="1" value={this.state.currentTime} id="playback-progress" />
             <div className="progress-text">
               <span className="progress-done">{this.state.curtimetext}</span>
@@ -216,7 +259,7 @@ class Player extends Component {
             </div>
           </div>
 
-          <input onChange={(e) => { this.volumeHandler(e.target.value) }} type="range" className="volume-progress" name="volume-progress" min="0" max="1" step="0.05" value={this.state.volume} id="volume-progress" />
+          <input onChange={(e) => { this.volumeHandler(e.target.value) }} type="range" className={` ${this.state.majorFading} volume-progress`} name="volume-progress" min="0" max="1" step="0.05" value={this.state.volume} id="volume-progress" />
 
           <video onCanPlay={() => this.initiateVideoInfoHandler()} onClick={() => this.togglePlay()} className="player" id="videoPlayer" src={this.state.streamPath} type={this.state.mimeType} autoPlay>
           </video>
