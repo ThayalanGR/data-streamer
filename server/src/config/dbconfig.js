238b0ee558bfbca0f37b12e991a19074684828af
ThayalanGR <grthayalan18@gmail.com>
2019-12-27 17:26:12 +0530

    dependency bugs fixed

--- server/src/config/dbconfig.js
+++ server/src/config/dbconfig.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 // const Uri = 'mongodb://<username>:<password>@ds247944.mlab.com:47944/apctmd';
 const Uri = 'mongodb://localhost/datastreamer';
 export default () => {
-    mongoose.connect(Uri, {useNewUrlParser: true});
+    mongoose.connect(Uri, {useNewUrlParser: true, useUnifiedTopology: true});
     mongoose.connection.on('connected', () => console.log('mongodb connected'));
     mongoose.connection.on('error', (err) => console.log(err));
 };
